52032b6fa8339ea0e312e2eb9bf869ca7835952f
Zshandi Krahn <ZshandiK@gmail.com>
2025-10-24 01:02:12 -0700

    Got jump and air movement working

---
+++ character.gd
@@ -0,0 +1,58 @@
+extends CharacterBody2D
+
+var framerate := 60
+var scaling_factor := 1.5
+var speed_scale := framerate * scaling_factor
+var acceleration_scale := framerate * framerate * scaling_factor
+
+# Setting variables
+
+var top_speed := 6 * speed_scale
+
+var jump_force = 6.5 * speed_scale
+
+var air_acceleration_speed := 0.09375 * acceleration_scale
+
+var gravity_force := 0.21875 * acceleration_scale
+var top_falling_speed := 16 * speed_scale
+
+# State variables
+
+var ground_speed := 0.0
+var is_on_ground := false
+
+func _physics_process(delta: float) -> void:
+    var is_left_pressed := Input.is_action_pressed("ui_left")
+    var is_right_pressed := Input.is_action_pressed("ui_right")
+    var is_jump_pressed := Input.is_action_pressed("ui_accept")
+
+    is_on_ground = is_on_floor()
+
+    if is_on_ground:
+        var ground_angle = 0 # TODO Calculate ground angle in degrees
+
+        if is_jump_pressed:
+            velocity.x -= jump_force * sin(deg_to_rad(ground_angle));
+            velocity.y -= jump_force * cos(deg_to_rad(ground_angle));
+        # TODO: Ground movement
+    else:
+        # Movement
+        if is_left_pressed:
+            velocity.x -= air_acceleration_speed * delta
+            if velocity.x < -top_speed:
+                velocity.x = - top_speed
+        if is_right_pressed:
+            velocity.x += air_acceleration_speed * delta
+            if velocity.x > top_speed:
+                velocity.x = top_speed
+
+        # Drag factor
+        # if velocity.y < 0 && velocity.y > -4:
+        #     velocity.x -= (velocity.x / 256); # May need to update to use "div"?
+
+        # Apply gravity
+        velocity.y += gravity_force * delta
+        if velocity.y > top_falling_speed:
+            velocity.y = top_falling_speed
+    
+    move_and_slide()
