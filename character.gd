109d8c5c5d109c20b9ed6165de4116ae9c2c5bb5
Zshandi Krahn <ZshandiK@gmail.com>
2025-10-25 10:09:30 -0700

    Got it working, but one more bug where you disappear when going off an edge

--- character.gd
+++ character.gd
@@ -32,7 +32,13 @@ var ground_distance := 5
 # State variables
 
 var ground_speed := 0.0
-var ground_angle := 0.0
+
+var ground_angle_rad := 0.0
+var ground_angle := 0.0:
+    set(value):
+        ground_angle = value
+        ground_angle_rad = deg_to_rad(value)
+
 var is_on_ground := false
 var is_jumping := false
 
@@ -42,6 +48,9 @@ var facing_dir_scale := 1
 var ground_sensors := [%GroundSensor1, %GroundSensor2, %GroundSensor3]
 
 func _physics_process(delta: float) -> void:
+    var current_position3 := global_position
+    if current_position3.y > 1000:
+        current_position3 = global_position
     var is_left_pressed := Input.is_action_pressed("ui_left")
     var is_right_pressed := Input.is_action_pressed("ui_right")
     var is_jump_pressed := Input.is_action_pressed("ui_accept")
@@ -56,8 +65,8 @@ func _physics_process(delta: float) -> void:
         facing_dir_scale = 1
 
     if is_on_ground and is_jump_pressed:
-        velocity.x -= jump_speed * sin(deg_to_rad(ground_angle));
-        velocity.y -= jump_speed * cos(deg_to_rad(ground_angle));
+        velocity.x -= jump_speed * sin(ground_angle_rad);
+        velocity.y -= jump_speed * cos(ground_angle_rad);
         is_jumping = true
         is_on_ground = false
 
@@ -82,8 +91,8 @@ func _physics_process(delta: float) -> void:
             if ground_speed_sign != sign(ground_speed):
                 ground_speed = 0
         
-        velocity.x = ground_speed * cos(ground_angle)
-        velocity.y = ground_speed * -sin(ground_angle)
+        velocity.x = ground_speed * cos(ground_angle_rad)
+        velocity.y = ground_speed * -sin(ground_angle_rad)
     else:
         # Movement
         if is_left_pressed:
@@ -108,8 +117,17 @@ func _physics_process(delta: float) -> void:
         if velocity.y > top_falling_speed:
             velocity.y = top_falling_speed
     
+    var current_position2 := global_position
+    if current_position2.y > 1000:
+        current_position2 = global_position
     _update_for_ground_angle()
+    var current_position := global_position
+    if current_position.y > 1000:
+        current_position = global_position
     move_and_slide()
+    var new_position := global_position
+    if new_position.y > 1000:
+        new_position = global_position
 
     var was_on_ground = is_on_ground
 
@@ -119,7 +137,8 @@ func _physics_process(delta: float) -> void:
         var total_normal = Vector2.ZERO
         var total_normal_count = 0
         for sensor in ground_sensors:
-            if sensor.is_colliding() and sensor.get_collision_depth() < 30:
+            if sensor.is_colliding() and sensor.get_collision_depth() < 15 and \
+                (not was_on_ground or abs(sensor.get_collision_normal().angle_to(up_direction)) < 50):
                 is_on_ground = true
                 total_normal += sensor.get_collision_normal()
                 total_normal_count += 1
@@ -135,20 +154,30 @@ func _physics_process(delta: float) -> void:
                 ground_angle += 360
             
             DebugValues.debug("avg_normal", avg_normal)
+
+        var m_velocity = velocity
+        var m_position = global_position
+        if total_normal_count == 0:
+            is_on_ground = is_on_ground
     
+    _update_for_ground_angle()
     if is_on_ground:
         _snap_downward()
 
         if not was_on_ground:
             # If we just landed, calculate the ground speed from the velocity
             ground_speed = velocity.length()
-            var dot = velocity.dot(Vector2.RIGHT.rotated(ground_angle))
+            var dot = velocity.dot(Vector2.RIGHT.rotated(ground_angle_rad))
             ground_speed *= sign(dot)
     
     DebugValues.debug("ground_speed", ground_speed)
     DebugValues.debug("ground_angle", ground_angle)
     DebugValues.debug("is_on_ground", is_on_ground)
     DebugValues.debug("velocity", velocity)
+    DebugValues.debug("global_position", global_position)
+    current_position3 = global_position
+    if current_position3.y > 1000:
+        current_position3 = global_position
 
 func _process(_delta: float) -> void:
     if is_jumping:
@@ -163,7 +192,7 @@ func _process(_delta: float) -> void:
 func _update_for_ground_angle():
     if not is_on_ground:
         ground_angle = 0
-    up_direction = Vector2.UP.rotated(deg_to_rad(ground_angle))
+    up_direction = Vector2.UP.rotated(ground_angle_rad)
     rotation_degrees = - ground_angle
 
 func _snap_downward():
@@ -171,4 +200,10 @@ func _snap_downward():
     var direction := Vector2.DOWN.rotated(rotation)
     var snap_velocity := direction * distance_to_snap
     
-    move_and_collide(snap_velocity)
+    var current_position := global_position
+
+    var collision = move_and_collide(snap_velocity)
+    
+    var new_position := global_position
+    if new_position.y > 1000:
+        collision = collision
