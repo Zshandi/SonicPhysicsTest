977434503cb6d8d1851fe7ff878f762cb1663a9a
Zshandi Krahn <ZshandiK@gmail.com>
2025-10-30 21:08:54 -0700

    Reduce repeated state transition setup

--- character_state_machine/state.gd
+++ character_state_machine/state.gd
@@ -38,3 +38,37 @@ func _physics_process(_delta: float) -> void:
 # Called for the current state when rendering (i.e. just called from _process)
 func _process(_delta: float) -> void:
 	pass
+
+class Group:
+	extends RefCounted
+
+	var states: Array[State]
+
+	func _init(st1_or_list: Variant, st2: State = null, st3: State = null, st4: State = null) -> void:
+		if st1_or_list is State:
+			states.push_back(st1_or_list)
+		else:
+			for st in st1_or_list:
+				states.push_back(st)
+		
+		if st2 != null:
+			states.push_back(st2)
+		if st3 != null:
+			states.push_back(st3)
+		if st4 != null:
+			states.push_back(st4)
+	
+	## condition should be a Callable or function name
+	func add_transition(to_state: State, condition: Variant) -> void:
+		assert(condition is Callable or condition is String)
+		if condition is Callable:
+			for st in states:
+				st.add_transition(to_state, condition)
+		elif condition is String:
+			for st in states:
+				st.add_transition(to_state, Callable(st, condition))
+	
+	func set_all(property: StringName, value: Variant) -> void:
+		for st in states:
+			if property in st:
+				st.set(property, value)
